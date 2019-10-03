4b200c4a9d201466795fbfa69cb2f24165582ced
tehnarenok <tehnarenok@gmail.com>
2019-10-03 23:05:44 +0300

    Updates

---
+++ precache-manifest.307fd9f9c24d829696d43f4b4f706d0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b71a722e9bf9198e580d",
+    "url": "/tehlin/static/js/runtime~main.06d702c7.js"
+  },
+  {
+    "revision": "dfddb9359ca5edff3769",
+    "url": "/tehlin/static/js/main.b6f00e09.chunk.js"
+  },
+  {
+    "revision": "0bf7edcc6bb89015597e",
+    "url": "/tehlin/static/js/2.24f3a0cf.chunk.js"
+  },
+  {
+    "revision": "0bf7edcc6bb89015597e",
+    "url": "/tehlin/static/css/2.6d845f31.chunk.css"
+  },
+  {
+    "revision": "d354fd5b8f3f8168c5f5db95bd2dd758",
+    "url": "/tehlin/index.html"
+  }
+];
