31ff572c70a28cced36548cac4b34fcd1acae4d0
tehnarenok <tehnarenok@gmail.com>
2019-10-03 11:45:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import connect from '@vkontakte/vk-connect';
+import { View } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+import Home from './panels/Home';
+import Persik from './panels/Persik';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			activePanel: 'home',
+			fetchedUser: null,
+		};
+	}
+
+	componentDidMount() {
+		connect.sendPromise('VKWebAppGetUserInfo', {})
+			.then(data => {
+				this.setState({ fetchedUser: data})
+			})
+			.catch(error => {
+				console.log(error)
+			})
+	}
+
+	go = (e) => {
+		this.setState({ activePanel: e.currentTarget.dataset.to })
+	};
+
+	render() {
+		return (
+			<View activePanel={this.state.activePanel}>
+				<Home id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
+				<Persik id="persik" go={this.go} />
+			</View>
+		);
+	}
+}
+
+export default App;
